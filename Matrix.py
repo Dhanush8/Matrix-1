b6684a7e90f75c2d859453b7bf585e7c5219fe2d
Dhanush8 <kasundanushka503@gmail.com>
2021-09-01 23:21:20 +0530

    Initial Commit

---
+++ Matrix.py
@@ -0,0 +1,16 @@
+R=int(input("Enter the number of rows: "))
+C=int(input("Enter the number of columns: "))
+
+Matrix=[]
+print("Enter the elements row wise:")
+for i in range(R):
+  a=[]
+  for j in range(C):
+    a.append(int(input()))
+  Matrix.append(a)
+for i in range(R):
+  for j in range(C):
+   print(Matrix[i][j],end=" ")
+  print()
+
+
